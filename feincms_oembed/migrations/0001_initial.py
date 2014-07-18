90eef59b997c97310161216baeb1570ab01a8df3
Matthias Kestenholz <mk@spinlock.ch>
2014-07-18 14:56:09 +0200

    Add Django 1.7-compatible migrations

--- feincms_oembed/migrations/0001_initial.py
+++ feincms_oembed/migrations/0001_initial.py
@@ -1,42 +1,31 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'CachedLookup'
-        db.create_table('feincms_oembed_cachedlookup', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('hash', self.gf('django.db.models.fields.CharField')(unique=True, max_length=40)),
-            ('url', self.gf('django.db.models.fields.URLField')(max_length=1000)),
-            ('_response', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-            ('_httpstatus', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
-            ('max_age_seconds', self.gf('django.db.models.fields.PositiveIntegerField')(default=604800)),
-            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-        ))
-        db.send_create_signal('feincms_oembed', ['CachedLookup'])
+    dependencies = [
+    ]
 
-    def backwards(self, orm):
-        # Deleting model 'CachedLookup'
-        db.delete_table('feincms_oembed_cachedlookup')
-
-    models = {
-        'feincms_oembed.cachedlookup': {
-            'Meta': {'object_name': 'CachedLookup'},
-            '_httpstatus': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            '_response': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            'hash': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '40'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'max_age_seconds': ('django.db.models.fields.PositiveIntegerField', [], {'default': '604800'}),
-            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'url': ('django.db.models.fields.URLField', [], {'max_length': '1000'})
-        }
-    }
-
-    complete_apps = ['feincms_oembed']
+    operations = [
+        migrations.CreateModel(
+            name='CachedLookup',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('hash', models.CharField(help_text='SHA-1 hash of the URL.', unique=True, max_length=40, verbose_name='hash')),
+                ('url', models.URLField(max_length=1000, verbose_name='URL')),
+                ('_response', models.TextField(null=True, blank=True)),
+                ('_httpstatus', models.PositiveIntegerField(null=True, blank=True)),
+                ('max_age_seconds', models.PositiveIntegerField(default=604800, verbose_name='Max. age in seconds')),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name='created')),
+                ('modified', models.DateTimeField(auto_now=True, verbose_name='modified')),
+            ],
+            options={
+                'verbose_name': 'cached lookup',
+                'verbose_name_plural': 'cached lookups',
+            },
+            bases=(models.Model,),
+        ),
+    ]
