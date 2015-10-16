2d9505c8d44b7e0826830cdb0d540476d506e1c6
Simon Baechler <sb@feinheit.ch>
2015-10-16 14:28:31 +0200

    make key mandatory for oembed

--- feincms_oembed/providers.py
+++ feincms_oembed/providers.py
@@ -1,12 +1,15 @@
 from django.utils.http import urlencode
+from django.conf import settings
 
 
 def embedly_oembed_provider(url, kwargs):
     """
     Provider for the oEmbed service at http://embed.ly/
+    Embedly requires an API key.
     """
     kwargs['url'] = url
-    return 'http://api.embed.ly/1/oembed?%s' % urlencode(kwargs)
+    kwargs['key'] = settings.EMBEDLY_KEY
+    return 'https://api.embed.ly/1/oembed?%s' % urlencode(kwargs)
 
 
 def noembed_oembed_provider(url, kwargs):
